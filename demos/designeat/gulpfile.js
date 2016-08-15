8474baf04fdbfbecc2b6ce9396f2a5ea5770e4cf
xuke <xuke.0412@qq.com>
2016-08-15 19:57:29 +0800

    html min

--- demos/designeat/gulpfile.js
+++ demos/designeat/gulpfile.js
@@ -2,9 +2,9 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-clean-css'),
+    htmlmin = require('gulp-htmlmin'),
     imagemin = require('gulp-imagemin');
  
-
 gulp.task('jsmin',function(){
     gulp.src(['js/unit.js','js/index.js'])
     .pipe(uglify())
@@ -16,6 +16,23 @@ gulp.task('cssmin',function(){
     .pipe(cssmin())
     .pipe(gulp.dest('css/'));
 });
+
+gulp.task('htmlmin', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('view/*.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('./'));
+});
+
 
 gulp.task('imagemin', function () {
     gulp.src('images/*.{png,jpg,gif,ico}')
