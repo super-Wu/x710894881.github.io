9fe4cfa8af80c3761e587d9f4c56af01f14b3ae7
xuke <xuke.0412@qq.com>
2016-11-20 17:10:46 +0800

    none

---
+++ function/function.js
@@ -0,0 +1,22 @@
+/*
+*
+*  一些小的函数
+*
+*/
+
+/*
+    生成一定数量的li
+*/ 
+/*
+    @参数说明：len:生成li的个数
+    @用法：creatLi(5);
+*/
+function creatLi(len){
+    return new Array(len+1).join('<li></li>');
+}
+
+
+
+
+
+
