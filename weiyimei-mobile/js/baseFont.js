91920a320dbfc1f4764456caeab16315a1a5e470
xuke <xuke.0412@qq.com>
2016-12-04 18:49:44 +0800

    add

---
+++ weiyimei-mobile/js/baseFont.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-11-17 00:22:37
+ * @version $Id$
+ */
+
+document.getElementsByTagName('html')[0].style.fontSize = document.documentElement.clientWidth / 10 + 'px';
+
+
